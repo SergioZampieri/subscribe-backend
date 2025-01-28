53c5dcadf3e674b79aa2a1b02e92e0e10eca3fec
Sergio <gersio.zampieri@gmail.com>
2025-01-27 23:57:15 -0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { MikroORM } from '@mikro-orm/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: 'Content-Type, Accept',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
   await app.get(MikroORM).getSchemaGenerator().ensureDatabase();
   await app.get(MikroORM).getSchemaGenerator().updateSchema();
   await app.listen(3001);
